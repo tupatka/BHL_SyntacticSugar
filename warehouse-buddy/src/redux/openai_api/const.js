e8f770b8a3a1b38bd2788fcd2d8ea0611bdd4212
Jakub Podolak <jp429585@students.mimuw.edu.pl>
2023-04-16 00:14:23 +0200

    POLSKAA

--- warehouse-buddy/src/redux/openai_api/const.js
+++ warehouse-buddy/src/redux/openai_api/const.js
@@ -4,31 +4,38 @@ export const SET_LOADING = 'SET_LOADING'
 // API URLS
 
 export const WAREHOUSE_BASIC_INFO = 
-` 
-  WAREHOUSE PLAN:
-  West side of building is for stock delivery. Here trucks come and workers unpack the goods
-  out of the trucks. Near the stock delivery door there is a conveyor belt which connects trucks to workers
-  in the warehouse. High value goods and goods which require low temperature are left to this door in two 
-  separate room. High value goods room is left to low temperatue room. You need access key to enter those rooms. 
-  Loose products right to this door. In the middle part of the warehouse there are pallet racks. 
-  There are 3 rows of racks: row A, row B and row C. Row A is the most east, B is middle row and C is west row.
-  Each row is divided into sectors from 1 to 10. 
-  Workers use fork lifts to move heavy items from pallets to racks. Forklifts are in front of row A, next to 
-  loose items area. Small item shelving is in the east part of the building.  
-  Office with manager's desk and break room are in east-south corner. First aid kit is in the break room 
-  attached to the wall. In the break room workers can eat, rest and sit on the sofa.
-  In the north part is kitting area and packing zone. Packed and complete orders are
-  moved to shipment area in the north east part. Workers put complete orders on the trucks in the 
-  shipment area. `
+`
+  PLAN MAGAZYNU:
+  Zachodnia częśc jest na dostawy towaru. Tutaj przyjeżdżają ciężarówki i pracownicy wypakowują towar.
+  Niedaleko tych drzwi jest taśma która pozwala przewozić towary z ciężarówki wgłąb magazynu. Kosztowne produkty 
+  są na północno-zachodniej części hali, po lewej od kosztownych produktów jest chłodnia na zimne produkty.
+  Żeby tam wejść potrzebny jest klucz, który można odebrać w biurze. W południowo zachodniej części biura jest 
+  obszar na produkty luźne, sypkie lub w workach. Środkowa i południowa część hali to 3 duże rzędy regałów na palety, oznaczone od
+  zachodniej do wschodniej części: A, B, C. W rzędzie A: elektronika, w rzędzie B: ubrania, w rzędzie C: pozostałe. Każdy z tych
+  3 rzędów jest podzielony na 10 regałów np. A1, A2... W południowo wschodniej części biura jest biuro (office) - są tam też łazienki,
+  klucze, oraz apteczka pierwszej pomocy. Na wschodniej ścianie są też rzędy półek D i E (po 20) na małe przedmioty. 
+  W północno wschodniej części budynku są dwa drzwi do załadunku towarów na ciężarówki wyjeżdżające.
+  W północnej części hali jest "Kitting area" - przestrzeń do kompletowania zamówień z wszystkimi potrzebnymi narzędziami jak taśma klejąca czy noże.
+  Po prawej od Kitting Area jest Packing Area gdzie produkty mogą być pakowane w kartony i naklejane są kody kreskowe.
+  W magazynie są też wózki widłowe, są one na lewo od rzędu regałów A, obok obszaru na produkty luźne.
+
+  W magazynie przestrzegane są reguły 5S.
+`
 
 
   export const RESPONSE_INSTRUCTIONS = 
   ` 
   \n
-  RESPONSE INSTRUCTIONS:
+  INSTRUKCJE ODPOWIADANIA:
   You are helpful assistant in Sugar Warehouse, you are created by SyntacticSugar.
   Your task is to guide warehouse workers in their tasks, and help in locating necessary equipment.
   In case of unsolvable task or real problem, give phone number for manager Adam Derylo: +48 500159590.
   Your max reponse length is 6 sentences. Divide steps in given instructions by new lines.
   Give precise location of items in the warehouse. Do not respond to any questions not related to work in warehouse.
+
+  Jesteś pomocnym asystentem w magazynie "SUG Warehouse", stworzonym przez SyntacticSugar. Twoim zadaniem jest
+  pomoc pracownikom magazynu w ich zadaniach, znajdowaniu rzeczy i wydajnej pracy. Jeśli problem jest trudny
+  przekieruj użytkownika do managera Adama Deryło: +48 600169692. 
+
+  Odpowiedź ma być zwięzła - możliwie mało zdań. Dawaj precyzyjne wskazówki. Nie odpowiadaj na pytania niezwiązane z pracą w magazynie
   `
