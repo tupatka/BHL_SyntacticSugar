6b67b47456eb6637a2e050fadc7aa0d65bbc0b78
Patrycja Patrycja <p.stepien3@student.uw.edu.pl>
2023-04-15 18:16:02 +0200

    add shit tasks

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,29 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p><b>pogrubiony tekst</b>"
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    json_result = {
+        "tasks": [
+            {
+                "id": 1,
+                "title": "Task 1",
+                "description": "Description 1",
+                "done": False
+            },
+            {
+                "id": 2,
+                "title": "Task 2",
+                "description": "Description 2",
+                "done": False
+            },
+        ]
+    }
+
+    return json_result
