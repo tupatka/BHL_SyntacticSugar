8e6f2444dbd7f377d33c5e9806e8ddd60ba905a5
Adam Derylo <a.m.derylo@gmail.com>
2023-04-15 20:42:57 +0200

    Add buttons to quick promt section

--- warehouse-buddy/src/containers/HomeView/FastPromptSection/index.js
+++ warehouse-buddy/src/containers/HomeView/FastPromptSection/index.js
@@ -1,9 +1,18 @@
 import './index.css';
+import { Flex, Button, Stack, Box, ButtonGroup, Wrap, WrapItem } from "@chakra-ui/react";
+
 
 export const FastPromptSection = () => {
     return (
         <div class="fast-prompt-container">
-            There are prompts
+                    <Flex  alignItems="center" justifyContent="center">
+            <ButtonGroup gap="4">
+              <Button colorScheme="yellow">WhiteAlpha</Button>
+              <Button colorScheme="blue">BlackAlpha</Button>
+            </ButtonGroup>
+            </Flex>
+
         </div>
+
     );
 }
