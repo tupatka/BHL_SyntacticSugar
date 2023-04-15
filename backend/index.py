9b3ec38d80eecede4ea2ce4eaba1f85bfa39d9cb
Patrycja Patrycja <p.stepien3@student.uw.edu.pl>
2023-04-16 01:34:10 +0200

    add ticketing to backend and db

--- backend/index.py
+++ backend/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import json
 app = Flask(__name__)
@@ -39,3 +39,29 @@ def get_fast_prompts():
          fast_prompts = json.dumps(json_result["fast_prompts"])
 
     return fast_prompts    
+
+@app.route('/ticketing', methods=['GET', 'POST'])
+@cross_origin()
+def ticketing():
+
+    if request.method == 'POST': 
+        new_ticket = request.get_json()
+
+        with open('db.json', 'r') as open_file:
+         json_object = json.load(open_file)
+         print(json_object["tickets"])
+         last_id = json_object["tickets"][-1]["id"]
+         
+         print(last_id)
+         new_ticket["id"] = last_id + 1
+         json_object["tickets"].append(new_ticket)
+         
+         with open('db.json', 'w') as db_file:
+            json.dump(json_object, db_file, indent=4)
+
+        return "OK"
+    else:      
+        with open('db.json', 'r') as open_file:
+            json_result = json.load(open_file)
+            tickets = json.dumps(json_result["tickets"])
+            return tickets      
