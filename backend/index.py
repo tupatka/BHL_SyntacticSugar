27c1db5b6a0f19e7c9e8d47526e0383f366295d2
Adam Derylo <a.m.derylo@gmail.com>
2023-04-16 06:19:30 +0200

    I hate 64base encoding with matadatahidden in bytes

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 import json
+import os
+import base64
+
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -39,3 +43,23 @@ def get_fast_prompts():
          fast_prompts = json.dumps(json_result["fast_prompts"])
 
     return fast_prompts    
+
+
+@app.route('/save_image', methods=['POST', 'GET'])
+@cross_origin()
+def save_image():
+    import base64
+    from io import BytesIO
+    from PIL import Image
+ 
+    data = json.loads(request.data.decode())
+    id = data["id"]
+    data = data["image_data"]
+
+    starter = data.find(',')
+    image_data = data[starter+1:]
+    image_data = bytes(image_data, encoding="utf-8")
+    im = Image.open(BytesIO(base64.b64decode(image_data)))
+    im.save('images/image' + str(id) +'.jpeg')
+    
+    return 'OK'
