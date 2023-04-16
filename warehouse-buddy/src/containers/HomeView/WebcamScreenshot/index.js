d0c4b6e9c7bf7a5fae457cb9788a44c3674e1f48
Adam Derylo <a.m.derylo@gmail.com>
2023-04-16 06:20:22 +0200

    I hate 64base encoding with matadatahidden in bytes

--- warehouse-buddy/src/containers/HomeView/WebcamScreenshot/index.js
+++ warehouse-buddy/src/containers/HomeView/WebcamScreenshot/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam"
-import { Button} from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
 export const WebcamScreenshot = () => {
 
@@ -14,14 +14,37 @@ export const WebcamScreenshot = () => {
     facingMode: "user",
   };
 
+  let user_id = 1;
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImg(imageSrc);
+
+    let apiUrl = 'http://localhost:5000/save_image';
+    let imageData = imageSrc.replace(/^data:image\/png;base64,/, '');
+    let req = {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'No-Auth': 'True'
+      },
+      body: JSON.stringify({ image_data: imageData, id: user_id }),
+    }
+    user_id = user_id + 1;
+    fetch(apiUrl, req)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, [webcamRef]);
 
-    return (
-    <div className="webcam-container">
 
+  return (
+    <div className="webcam-container">
       {img === null ? (
         <>
           <Webcam
